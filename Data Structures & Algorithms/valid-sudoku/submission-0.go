5600eacb9f8364c946e32c45d2ed55cc1b427d64
Edwin2345 <112472516+Edwin2345@users.noreply.github.com>
2026-04-29 15:49:00 -0400

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.go
@@ -0,0 +1,68 @@
+/*
+QQ
+1. so the board down need to be filled for it to be valid, as logn as we dont duplicate its fine?
+2. fixed board size?
+
+
+Plan
+1. seperate functions to check rows, columns, squares
+2. use a set in each function to check for duplicates
+
+*/
+
+func isValidRows(board [][]byte) bool {
+    for r := 0; r < 9; r++ {
+        rowSet := make(map[byte]bool)
+        for c := 0; c < 9; c++ {
+            if board[r][c] == '.' {
+                continue
+            }
+            if rowSet[board[r][c]] {
+                return false
+            }
+            rowSet[board[r][c]] = true
+        }
+    }
+    return true
+}
+
+func isValidCols(board [][]byte) bool {
+    for c := 0; c < 9; c++ {
+        colSet := make(map[byte]bool)
+        for r := 0; r < 9; r++ {
+            if board[r][c] == '.' {
+                continue
+            }
+            if colSet[board[r][c]] {
+                return false
+            }
+            colSet[board[r][c]] = true
+        }
+    }
+    return true
+}
+
+func isValidSquares(board [][]byte) bool {
+    for rightCornerRow := 0; rightCornerRow <= 6; rightCornerRow += 3 {
+        for rightCornerCol := 0; rightCornerCol <= 6; rightCornerCol += 3 {
+            squareSet := make(map[byte]bool)
+            for r := rightCornerRow; r <= rightCornerRow+2; r++ {
+                for c := rightCornerCol; c <= rightCornerCol+2; c++ {
+                    if board[r][c] == '.' {
+                        continue
+                    }
+                    if squareSet[board[r][c]] {
+                        return false
+                    }
+                    squareSet[board[r][c]] = true
+                }
+            }
+        }
+    }
+    return true
+}
+
+
+func isValidSudoku(board [][]byte) bool {
+     return isValidRows(board) && isValidCols(board) && isValidSquares(board)
+}
