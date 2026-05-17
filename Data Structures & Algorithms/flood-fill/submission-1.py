24abd16d192db881c60536efad76e25e534fef0f
Edwin2345 <112472516+Edwin2345@users.noreply.github.com>
2026-05-17 16:54:24 -0400

    Add: flood-fill - submission-1

---
+++ Data Structures & Algorithms/flood-fill/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        ROWS,COLS = len(image), len(image[0])
+        visited = set()
+        origColor = image[sr][sc]
+
+
+        def dfs(r,c):
+            #base cases
+            if min(r,c) < 0 or r >= ROWS or c >= COLS:
+               return
+            if image[r][c] != origColor or (r,c) in visited:
+               return
+
+            #update color in current square
+            image[r][c] = color
+            visited.add((r,c))
+
+            #recurse to neightbors
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+
+        dfs(sr,sc)
+        return image 
+
