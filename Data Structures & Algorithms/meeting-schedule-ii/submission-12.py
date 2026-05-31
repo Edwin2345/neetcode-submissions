70ee8b4937483d5cc78e652cbd455e2336d3835f
Edwin2345 <112472516+Edwin2345@users.noreply.github.com>
2026-05-30 22:10:34 -0400

    Add: meeting-schedule-ii - submission-12

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-12.py
@@ -0,0 +1,27 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        #scan the line, keep track of max
+        maxMeetingRoomsNeeded = 0
+        timeline = []
+        for i in intervals:
+            timeline.append( (i.start,1) )
+            timeline.append( (i.end,-1) )
+        
+        #sort timeline by time
+        timeline.sort()
+
+        cur = 0
+        for time, change in timeline:
+            cur += change
+            maxMeetingRoomsNeeded = max(maxMeetingRoomsNeeded, cur)
+
+        return maxMeetingRoomsNeeded
+        
