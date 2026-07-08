47682538818c940e66878e224cf458ed1ed6f890
Edwin2345 <112472516+Edwin2345@users.noreply.github.com>
2026-07-08 13:17:44 -0400

    Add: implement-queue-using-stacks - submission-2

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-2.py
@@ -0,0 +1,70 @@
+class MyQueue:
+    """
+    s1: 1        -> add fist element to s1, and rest to s2
+    s2: 2,3,4,5
+
+    s1: 5,4,3,2  -> pop form s1 then transfer s2 to s1
+    s2: 
+
+    s1: 5,4,3,2 ->
+    s2: 6
+
+    s1: 5,4,3,2 
+    s2: 6,7
+
+    idea: alwasy keep s1 as correctly ordered
+         once s1 is empty, pop and push elements of s2 into s1
+
+         push:  if s1 has no element, add 
+                if it does, put into s2
+        
+         pop: if s1 empty, move all elements from s2 into s1
+              then pop from s1
+    """
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+        
+    def push(self, x: int) -> None:
+        if len(self.s1) == 0:
+           self.s1.append(x)
+        else:
+           self.s2.append(x) 
+
+    def pop(self) -> int:
+        if self.empty():
+           raise ValueError("queue is empty")
+        #if s1 is empty, move over s2 then pop
+        elif len(self.s1) == 0:
+            while len(self.s2) > 0:
+                num = self.s2.pop()
+                self.s1.append(num)
+            return self.s1.pop()
+        #if not, get the top of s1, and if empty move everything over
+        else:
+            firstEl = self.s1.pop()
+            if len(self.s1) == 0:
+               while len(self.s2) > 0:
+                   num = self.s2.pop()
+                   self.s1.append(num) 
+            return firstEl
+    
+
+    def peek(self) -> int:
+        if self.empty():
+            raise ValueError("queue is empty")
+        return self.s1[-1]
+
+    def empty(self) -> bool:
+        return len(self.s1) == 0 and len(self.s2) == 0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
