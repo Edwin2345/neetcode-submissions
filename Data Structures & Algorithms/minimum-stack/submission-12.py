77c0288e3aacf60090cfe44713a6e8f6482533bc
Edwin2345 <112472516+Edwin2345@users.noreply.github.com>
2026-07-08 10:14:41 -0400

    Add: minimum-stack - submission-12

---
+++ Data Structures & Algorithms/minimum-stack/submission-12.py
@@ -0,0 +1,23 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+    
+    def push(self, val):
+        self.stack.append(val)
+
+        if len(self.minStack) == 0:
+            self.minStack.append(val)
+        
+        elif self.minStack[-1] >= val:
+            self.minStack.append(val)
+    
+    def pop(self):
+        if self.stack.pop() == self.minStack[-1]:
+            self.minStack.pop()
+    
+    def top(self):
+        return self.stack[-1]
+    
+    def getMin(self):
+        return self.minStack[-1]
