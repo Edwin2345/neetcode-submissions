b00f204860c554fb6032b0e0be73bfb6cf6b56f9
Edwin2345 <112472516+Edwin2345@users.noreply.github.com>
2026-07-15 22:42:13 -0400

    Add: find-target-in-rotated-sorted-array - submission-8

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-8.py
@@ -0,0 +1,33 @@
+class Solution:
+   #rotated sorted creates two sorted halfs LSH and RSH
+   #we are LSH if nums[L] <= nums[mid], else RSH
+   #do a binary search, but if we are not in correct half, go to other
+   def search(self, nums: List[int], target: int) -> int:
+       L = 0
+       R = len(nums) - 1
+
+       while L <= R:
+         #compute mid index and check if its target
+         mid = L + (R - L)//2
+         if nums[mid] == target:
+            return mid
+
+         #LSH
+         if nums[L] <= nums[mid]:
+            #we are in correct half     
+            if nums[L] <= target < nums[mid]:
+               R = mid - 1
+            #otherwise, go right
+            else:
+               L = mid + 1          
+         #RSH
+         else:
+            #we are in correct half
+            if nums[mid] < target <= nums[R]:
+               L =  mid + 1
+            #otherwise go left
+            else:
+               R = mid -1
+
+       return -1
+        
