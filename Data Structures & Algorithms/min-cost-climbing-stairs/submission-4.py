b8aa314803b461d4af08a0e9518ee2b493d4660b
Edwin2345 <112472516+Edwin2345@users.noreply.github.com>
2026-08-31 12:18:12 -0400

    Add: min-cost-climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        #Top Down DP: memoize the min cost at a certain step value to reahc top
+        cache = {}
+        def calcMinCost(step):
+            #reached top of stairs
+            if step == len(cost):
+               return 0            
+            #at the last step, only choise is to pay that cost and go one step up
+            if step == len(cost)-1:
+               return cost[step]
+            #already compute min cost to reach top at this step value
+            if step in cache:
+               return cache[step]
+            
+            #otherwise, cost is current step + min of choosing 1 or 2 steps
+            cache[step] = cost[step] + min(
+               calcMinCost(step + 1), 
+               calcMinCost(step + 2)
+            )
+
+            return cache[step]
+            
+        #return the min stating from either index 0 or index 1
+        return min(calcMinCost(0), calcMinCost(1))
