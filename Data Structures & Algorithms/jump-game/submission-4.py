129af2c418bf7655ba163d41ee971d1a64c293d5
Edwin2345 <112472516+Edwin2345@users.noreply.github.com>
2026-09-06 22:55:11 -0400

    Add: jump-game - submission-4

---
+++ Data Structures & Algorithms/jump-game/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    # O(N) Time, O(N) Space approach -> bfs with queue + visited set
+    def canJump(self, nums: List[int]) -> bool:
+        q = deque([0])
+        visit = set([0])
+
+        while len(q) > 0:
+            #check if curr index is at end
+            i = q.popleft()
+            if i == len(nums) - 1:
+               return True
+            
+            #add all reachable indices to queue
+            for j in range(1, nums[i] + 1):
+                newIndex = i + j
+                if newIndex >= len(nums):
+                   break
+                elif newIndex not in visit:
+                   q.append( newIndex ) 
+                   visit.add( newIndex )
+
+        return False 
+        
